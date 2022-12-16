7d6c8a26da9c741e88c76102c92128f0a84479a8
sanket-16 <san162002@gmail.com>
2022-12-16 13:27:58 +0530

    basic boilerplate using stream api without css

---
+++ client/src/components/ChannelListContainer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ChannelListContainer() {
+	return <div>ChannelListContainer</div>;
+}
+
+export default ChannelListContainer;
---
+++ client/src/components/TeamChannelPreview.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Avatar, useChatContext } from 'stream-chat-react';
+
+function TeamChannelPreview({ channel, type }) {
+	const { channel: activeChannel, client } = useChatContext();
+    function DirectPreview() {
+        const members = Object.values(channel.state.members).filter(({user})=> user.id !== client.userID)
+        return (
+            <div>
+            <Avatar image={members[0]?.user?.image} name={members[0]?.user?.fullName} size={24} />
+            <p>name={members[0]?.user?.fullName}</p>
+            </div>
+        )
+    }
+	function ChannelPreview() {
+        return (
+            <p>
+                # {channel?.data?.name || channel?.data?.id}
+            </p>
+            );
+    }
+
+    return (
+        <div onClick={()=>{console.log(channel)}}>
+            {type === 'team' ? <ChannelPreview /> : <DirectPreview/>}
+        </div>
+    )
+}
+
+
+export default TeamChannelPreview;
