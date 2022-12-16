7777245bac2e3120557c1996c1dc10ecfe891c27
OmkarGurav <omkargurav1920@gmail.com>
2022-12-16 18:16:28 +0530

    FIX:double channel⚒

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -1,34 +1,53 @@
-import React from 'react';
-import { ChannelList, useChatContext } from 'stream-chat-react';
-import ChannelSearch from './ChannelSearch';
-import TeamChannelList from './TeamChannelList';
-import TeamChannelPreview from './TeamChannelPreview';
+// import React from 'react';
+// import { ChannelList, useChatContext } from 'stream-chat-react';
+// import ChannelSearch from './ChannelSearch';
+// import TeamChannelList from './TeamChannelList';
+// import TeamChannelPreview from './TeamChannelPreview';
 
-function Sidebar() {
-	return <div>Sidebar</div>;
-}
-function Header() {
-	return <div>Header</div>;
-}
+// function Sidebar() {
+// 	return <div>Sidebar</div>;
+// }
+// function Header() {
+// 	return <div>Header</div>;
+// }
+// function ChannelContainer() {
+// 	return (
+// 		<>
+// 			<Sidebar />
+// 			<div>
+// 				<Header />
+// 				<ChannelSearch />
+// 				<ChannelList
+// 					filters={{}}
+// 					channelRenderFilterFn={() => {}}
+// 					List={(listProps) => (
+// 						<TeamChannelList {...listProps} type='team' />
+// 					)}
+// 					Preview={(previewProps) => (
+// 						<TeamChannelPreview {...previewProps} type='team' />
+// 					)}
+// 				/>
+// 			</div>
+// 		</>
+// 	);
+// }
+
+// export default ChannelContainer;
+
+
+
+
+import React from 'react';
+
+
 function ChannelContainer() {
+
+
 	return (
-		<>
-			<Sidebar />
-			<div>
-				<Header />
-				<ChannelSearch />
-				<ChannelList
-					filters={{}}
-					channelRenderFilterFn={() => {}}
-					List={(listProps) => (
-						<TeamChannelList {...listProps} type='team' />
-					)}
-					Preview={(previewProps) => (
-						<TeamChannelPreview {...previewProps} type='team' />
-					)}
-				/>
-			</div>
-		</>
+		<div>
+
+			<div>Channel container</div>
+		</div>
 	);
 }
 
--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,7 +1,88 @@
 import React from 'react';
+import { ChannelList } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
 
-function ChannelListContainer() {
-	return <div>ChannelListContainer</div>;
+import ChannelSearch from './ChannelSearch';
+import TeamChannelList from './TeamChannelList';
+import TeamChannelPreview from './TeamChannelPreview';
+
+
+const cookies = new Cookies();
+
+const SideBar = () => (
+	<div >
+		<div >
+			<div >
+				Company logo
+			</div>
+		</div>
+		<div >
+			<div >
+				logout
+			</div>
+		</div>
+	</div>
+);
+
+const CompanyHeader = () => (
+	<div>
+		<p >Stream </p>
+	</div>
+)
+
+
+
+const ChannelListContainer = () => {
+
+
+
+
+	return (
+		<>
+			<SideBar />
+			<div >
+				<CompanyHeader />
+				<ChannelSearch />
+				<ChannelList
+					filters={{}}
+					channelRenderFilterFn={() => { }}
+					List={(listProps) => (
+						<TeamChannelList
+							{...listProps}
+							type="team"
+
+						/>
+					)}
+					Preview={(previewProps) => (
+						<TeamChannelPreview
+							{...previewProps}
+
+							type="team"
+						/>
+					)}
+				/>
+				<ChannelList
+					filters={{}}
+					channelRenderFilterFn={() => { }}
+					List={(listProps) => (
+						<TeamChannelList
+							{...listProps}
+							type="messaging"
+
+						/>
+					)}
+					Preview={(previewProps) => (
+						<TeamChannelPreview
+							{...previewProps}
+
+							type="messaging"
+						/>
+					)}
+				/>
+			</div>
+		</>
+	);
 }
+
 
 export default ChannelListContainer;
