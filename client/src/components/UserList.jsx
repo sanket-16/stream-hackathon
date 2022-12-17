a09792d18bc0bd893423f5d0d34f62dd94cbd0e8
OmkarGurav <omkargurav1920@gmail.com>
2022-12-17 15:34:19 +0530

    Users List in Channel

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,12 +1,116 @@
 import React, { useEffect, useState } from 'react';
 import { Avatar, useChatContext } from 'stream-chat-react';
+import {FcInvite} from 'react-icons/fc'
 
-function UserList() {
-  return (
-    <div>
-    userlist
-    </div>
-  )
+
+
+
+
+const ListContainer = ({ children }) => {
+    return (
+        <div >
+            <div >
+                <p>User</p>
+                <p>Invite</p>
+            </div>
+            {children}
+        </div>
+    )
+}
+
+
+
+const UserItem = ({ user, setSelectedUsers }) => {
+    const [selected, setSelected] = useState(false)
+
+    const handleSelect = () => {
+        if(selected) {
+            setSelectedUsers((prevUsers) => prevUsers.filter((prevUser) => prevUser !== user.id))
+        } else {
+            setSelectedUsers((prevUsers) => [...prevUsers, user.id])
+        }
+
+        setSelected((prevSelected) => !prevSelected)
+    }
+
+    return (
+        <div  onClick={handleSelect}>
+            <div >
+                <Avatar image={user.image} name={user.fullName || user.username} size={32} />
+                <p >{user.fullName || user.id}</p>
+            </div>
+            {selected ? <FcInvite /> : <div />}
+        </div>
+    )
+}
+
+
+
+function UserList({ setSelectedUsers }) {
+    const { client } = useChatContext();
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [listEmpty, setListEmpty] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        const getUsers = async () => {
+            if(loading) return;
+
+            setLoading(true);
+            
+            try {
+                const response = await client.queryUsers(
+                    { id: { $ne: client.userID } },
+                    { id: 1 },
+                    { limit: 8 } 
+                );
+
+                if(response.users.length) {
+                    setUsers(response.users);
+                } else {
+                    setListEmpty(true);
+                }
+            } catch (error) {
+               setError(true);
+            }
+            setLoading(false);
+        }
+
+        if(client) getUsers()
+    }, []);
+
+    if(error) {
+        return (
+            <ListContainer>
+                <div >
+                    Error loading, please refresh and try again.
+                </div>
+            </ListContainer>
+        )
+    }
+
+    if(listEmpty) {
+        return (
+            <ListContainer>
+                <div >
+                    No users found.
+                </div>
+            </ListContainer>
+        )
+    }
+
+    return (
+        <ListContainer>
+            {loading ? <div >
+                Loading users...
+            </div> : (
+                users?.map((user, i) => (
+                  <UserItem index={i} key={user.id} user={user} setSelectedUsers={setSelectedUsers} />  
+                ))
+            )}
+        </ListContainer>
+    )
 }
 
 export default UserList
